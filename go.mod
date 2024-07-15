66a5b59b22e6515b72d75ccad732c7ebb39e1283
SlimyZockt <35144741+SlimyZockt@users.noreply.github.com>
2024-07-15 18:06:07 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module server
+
+require (
+	github.com/a-h/templ v0.2.731
+)
+
+go 1.21.6
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import daisyui from "daisyui";
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+}
+
